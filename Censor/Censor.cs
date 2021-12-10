8dd1b80b8f7b6bdccc68702508433fcb4a9ea94f
Maura Cerioli <maura.cerioli@unige.it>
2021-12-10 14:36:57 +0100

    Solution fatta a lezione

--- Censor/Censor.cs
+++ Censor/Censor.cs
@@ -1,9 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Censor {
     public interface I{
         string Message { get; }
     }
     public static class CensorClass {
+        public static IEnumerable<I> Censor(IEnumerable<I> sequence, string badWord){
+            if (null==sequence)
+                throw new ArgumentNullException(nameof(sequence), "cannot be null");
+            return SafeCensor();
+
+            IEnumerable<I> SafeCensor(){
+                /* NON FUNZIONA
+             1) perché se sequence è infinita e non contiene null Any non termina
+             2)  perché in ogni caso dovrei fare due enumerazioni, quella dell'Any e quella in cui li uso
+                e non ho nessuna garanzia che le due enumerazioni mi diano gli stessi risultati
+                quindi è un controllo che non controlla veramente
+             if (sequence.Any(x=>null==x)){
+                throw new ArgumentNullException();
+            }*/
+                foreach (var x in sequence){
+                    if (null == x){
+                        throw new ArgumentNullException(nameof(sequence), "cannot contain a null element");
+                    }
+
+                    if (!new Regex(badWord).IsMatch(x.Message)){
+                        yield return x;
+                    }
+                }
+            }
+        }
     }
 }
--- CensorTests/CensorTests.cs
+++ CensorTests/CensorTests.cs
@@ -1,6 +1,140 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Censor;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using Moq;
+using NUnit.Framework;
 
 namespace CensorTests {
+    // Da usare come stub invece di Moq
+    // oppure come mock per behavior test in cui
+    // si verifica che ogni elemento sia letto una sola volta
+    internal class MyI : I{
+        private readonly string _message;
+
+        public int MessageRead{ get; private set; }
+
+        internal MyI(string msg){
+            _message = msg;
+            MessageRead = 0;
+        }
+
+        public string Message{
+            get{
+                MessageRead++;
+                return _message; }
+        }
+    }
+    [TestFixture]
     public class CensorTests {
+        [TestCase()]
+        [TestCase("pippo")]
+        [TestCase("pippo","pluto")]
+        [TestCase("pippo","pluto","paperino")]
+        public void NothingToFilter(params string[] theMsgSequence){
+            var theSequence = new Mock<I>[theMsgSequence.Length];
+            for (int i = 0; i < theMsgSequence.Length; i++){
+                theSequence[i] = new Mock<I>();
+                theSequence[i].Setup(x => x.Message).Returns(theMsgSequence[i]);
+            }
+
+            var enumerable = theSequence.Select(m => m.Object);
+            var result = CensorClass.Censor(enumerable, "kkzt");
+            Assert.That(result,Is.EqualTo(enumerable));
+        }
+        /*
+         *Approccio privo di senso: EqualTo va in loop
+         * ..e non c'è modo di verificare elemento a elemento l'uguaglianza
+         */
+        public void NothingToFilterInfinite(){
+            IEnumerable<I> Infinite(){
+                while (true){
+                    var a = new Mock<I>();
+                    a.Setup(x => x.Message).Returns("puffo");
+                    yield return a.Object;
+                }
+            }
+
+            var enumerable = Infinite();
+            var result = CensorClass.Censor(enumerable, "kkzt");
+            Assert.That(result, Is.EqualTo(enumerable));
+        }
+
+        [TestCase(10)]
+        public void NothingToFilterInfiniteApprox(int approx){
+            var theSequence = CreateFiniteSequence(approx);
+
+            IEnumerable<I> Infinite(){
+                    for (int i = 0; i < approx; i++)
+                        yield return theSequence[i];
+                    while (true){
+                        var a = new Mock<I>();
+                        a.Setup(x => x.Message).Returns("puffo");
+                        yield return a.Object;
+                    }}
+
+                var result = CensorClass.Censor(Infinite(), "kkzt").Take(approx);
+                Assert.That(result,Is.EqualTo(theSequence));
+            }
+        private static I[] CreateFiniteSequence(string[] messages){
+            var theSequence = new I[messages.Length];
+            for (int i = 0; i < messages.Length; i++){
+                var a = new Mock<I>();
+                a.Setup(x => x.Message).Returns(messages[i]);
+                theSequence[i] = a.Object;
+            }
+
+            return theSequence;
+        }
+        private static I[] CreateFiniteSequence(int approx){
+            var theSequence = new I[approx];
+            for (int i = 0; i < approx; i++){
+                var a = new Mock<I>();
+                a.Setup(x => x.Message).Returns("puffo");
+                theSequence[i] = a.Object;
+            }
+
+            return theSequence;
+        }
+
+        // La sequenza di input contiene un unico elemento che contiene badWord in mezzo al messaggio
+        [Test]
+        public void OneMatchInTheMiddle(){
+            var badWord = "vai a quel paese";
+            var mock = new Mock<I>();
+            mock.Setup(x => x.Message).Returns($"jkashgahlkjgh {badWord}uihsehghek");
+            var result = CensorClass.Censor(new[]{ mock.Object }, badWord);
+            Assert.That(result,Is.Empty);
+        }
+        // individua ed elimina da una sequenza più elementi che contengono la bad word
+        [Test]
+        public void FilterManyElements(){
+            var badWord = "xxx";
+            var inputSequenceMsg = new[]
+                { "puffo", "topolino", $"prima{badWord}dopo", "paperino", $"{badWord}", $"{badWord} in testa" };
+            var myInput = CreateFiniteSequence(inputSequenceMsg);
+            //guardare sintassi per slicing e provare a fare refactoring
+            var expected = new I[3];
+            expected[0] = myInput[0];
+            expected[1] = myInput[1];
+            expected[2] = myInput[3];
+            var result = CensorClass.Censor(myInput, badWord);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void OnNullSequenceThrows(){
+            Assert.That(()=>CensorClass.Censor(null,"puffo"),Throws.TypeOf<ArgumentNullException>());
+        }
+        [Test]
+        public void OnSequenceWithNullThrows(){
+            var myInput = CreateFiniteSequence(5);
+            myInput[2] = null;
+            Assert.That(()=>CensorClass.Censor(myInput,"puffo").Take(3).Count(),Throws.TypeOf<ArgumentNullException>());
+        }
+
     }
 }
